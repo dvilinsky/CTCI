af8e9a1143f00395b1959d5c832063d6bf245f1a
Daniel V <dvilinsk@brandeis.edu>
2019-06-12 17:41:50 -0400

    Complete problem 2.8

--- src/chapter2/C2.java
+++ src/chapter2/C2.java
@@ -6,21 +6,29 @@ import java.util.Iterator;
 
 public class C2 {
     public static void main(String[] args) {
-        DoublyLinkedList<Integer> d = new DoublyLinkedList<>();
-        d.append(7);
-        d.append(1);
-        d.append(6);
-//        chapter2.DoublyLinkedList<Integer> l = new chapter2.DoublyLinkedList<>();
-        d.append(5);
-        d.append(9);
-        d.append(2);
-//        System.out.println(l.sumWith(d, true));
     }
 
     public static void printList(DoublyLinkedList d) {
         Iterator<Integer> itr = d.iterator();
         for (Object i : d) {
             System.out.println(i);
+        }
+    }
+
+    /**
+     * Problem 2.8. Given head of list, returns the start of the loop. Assumes given node has a loop somewhere
+     * @param node
+     * @return
+     */
+    public static SinglyLinkedNode startOfLoop(SinglyLinkedNode node) {
+        SinglyLinkedNode fast = node;
+        SinglyLinkedNode slow = node;
+        while (true) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return fast.next;
+            }
         }
     }
 }
