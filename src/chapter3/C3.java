40bbd55c32fe4fb169ad9c2bbbbdfdbe7e58f8c1
Daniel V <dvilinsk@brandeis.edu>
2019-06-19 17:26:18 -0400

    Implement push in ThreeStack

--- src/chapter3/C3.java
+++ src/chapter3/C3.java
@@ -2,9 +2,21 @@ package chapter3;
 
 public class C3 {
     public static void main(String[] args) {
-        MyStack<Integer> m = new MyStack<>();
-        for (int i = 0; i < 10; i++) {
-            m.push(i);
-        }
+        ThreeStack stack = new ThreeStack(10);
+        stack.push(1, 0);
+        stack.push(2, 0);
+        stack.push(3, 0);
+        stack.push(4, 0);
+        stack.push(5, 1);
+        stack.push(22, 1);
+        stack.push(19, 1);
+        stack.push(9, 2);
+        stack.push(56, 2);
+        stack.push(27, 2);
+        stack.push(6, 0);
+        stack.push(7, 0);
+        stack.push(11, 1);
+        stack.push(44, 2);
+        System.out.println(stack);
     }
 }
---
+++ src/chapter3/ThreeStack.java
@@ -0,0 +1,85 @@
+package chapter3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem 3.1
+ * Implements three stacks using one array. For proof of concept, it only accepts integers, but could be easily
+ * extended to generics (although java doesn't really care for generic arrays)
+ */
+public class ThreeStack {
+    private static final int DEFAULT_CAPACITY = 21;
+    private int[] backingArray;
+    private Map<Integer, Integer> tops;
+    private Map<Integer, Integer> limits;
+
+    public ThreeStack(int initialCapacity) {
+        backingArray = new int[initialCapacity];
+        tops = new HashMap<>();
+        limits = new HashMap<>();
+        tops.put(0, 0);
+        tops.put(1, initialCapacity/3 + 1);
+        tops.put(2, initialCapacity * 2 / 3 + 1);
+        updateLimits(backingArray);
+    }
+
+    public ThreeStack() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Pushes given data onto given stack
+     * @param data the data to be added to one of the stack
+     * @param stack the stack to which to add the data (must be between 0-2, inclusive)
+     */
+    public void push(int data, int stack) {
+        if (stack > 2 || stack < 0) {
+            throw new IndexOutOfBoundsException("Error; Invalid Stack");
+        }
+        int top = tops.get(stack);
+        if (isFull(stack, top)) {
+            expand();
+        }
+        backingArray[top++] = data;
+        tops.put(stack, top);
+    }
+
+    private boolean isFull(int stack, int top) {
+       return tops.get(stack) == limits.get(stack);
+    }
+
+    /**
+     * Expands the capacity of the backing array. This copies elements into the backing array into a new array
+     * of size 2 * backingArray.length. We expand every time any one of the stacks is full.
+     */
+    private void expand() {
+        int[] newArray = new int[backingArray.length * 2];
+        int index = 0;
+        int currentStack = 0;
+        for (int i = 0; i < backingArray.length; i++) {
+            if (currentStack != 2 && i == tops.get(currentStack)) {
+                tops.put(currentStack, index);
+                index = ((currentStack + 1) * newArray.length/3 + 1);
+                i = ((currentStack + 1) * backingArray.length/3 + 1);
+                currentStack++;
+            }
+            newArray[index++] = backingArray[i];
+        }
+        tops.put(currentStack, index);
+        backingArray = newArray;
+        updateLimits(backingArray);
+    }
+
+    private void updateLimits(int[] backingArray) {
+        limits.put(0, backingArray.length/3 + 1);
+        limits.put(1, backingArray.length * 2 / 3 + 1);
+        limits.put(2, backingArray.length);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(backingArray);
+    }
+}
